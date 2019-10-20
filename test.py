8983c48436c526d01481124119e1ba2b6b2cebd0
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2019-10-20 19:57:32 +0200

    Initial implementation of support for diffrent programming languages

--- test.py
+++ test.py
@@ -2,25 +2,62 @@ import os, subprocess
 
 from get import promptToGet
 
-def getBytesFromFile(file):
-    inFile = open(file, "rb")
-    result = inFile.read()
-    inFile.close()
-    return result
+_LANGUAGE_COMMANDS = {
+    '.py': ['python3', '@f'],
+    '.php': ['php', '@f'],
+    # TODO: Figure out how to pass class name to java and possibly compile the class beforehand
+    # '.java': ['java', '@c'],
+    # TODO: Support rest of the languages that kattis supports
+}
+
+_LANGUAGE_GUESS = {
+    '.c': 'C',
+    '.c#': 'C#',
+    '.c++': 'C++',
+    '.cc': 'C++',
+    '.cpp': 'C++',
+    '.cs': 'C#',
+    '.cxx': 'C++',
+    '.go': 'Go',
+    '.h': 'C++',
+    '.hs': 'Haskell',
+    '.java': 'Java',
+    '.js': 'JavaScript',
+    '.m': 'Objective-C',
+    '.pas': 'Pascal',
+    '.php': 'PHP',
+    '.pl': 'Prolog',
+    '.py': 'Python',
+    '.rb': 'Ruby'
+}
 
 def test(args, options):
-    if not os.path.exists(args[0]):
+    problemName = args[0]
+
+    if not os.path.exists(problemName):
         promptToGet(args, options)
         return
-    testPath = args[0] + "/test"
+    
+    testPath = problemName + "/test"
     files = [f for f in os.listdir(testPath) if os.path.isfile(os.path.join(testPath, f))]
     inFiles = [testPath + "/" + f for f in files if f.endswith(".in")]
     ansFiles = [testPath + "/" + f for f in files if f.endswith(".ans")]
     passed = True
+    
+    programFile = getSourceFile(problemName)
+    if(programFile == -1):
+        return
+    
+    command = getCommand(problemName, programFile)
+
+    print(command)
+
+    if(command == -1):
+        return
+
     for inF, ansF in zip(inFiles, ansFiles):
-        input = getBytesFromFile(inF)
         answer = getBytesFromFile(ansF).decode("utf-8")
-        result = subprocess.run(["python3", args[0]+"/"+args[0]+".py"], stdout=subprocess.PIPE, input=input).stdout.decode("utf-8").replace("\r\n", "\n")
+        result = runSingleTest(command, inF)
         if (answer == result):
             print("\U0001F49A", inF, "succeeded")
         else:
@@ -34,3 +71,50 @@ def test(args, options):
     
     if passed and "-a" in options:
         archive(args, options)
+
+def runSingleTest(command, inFile):
+    inp = getBytesFromFile(inFile)
+    return subprocess.run(command, stdout=subprocess.PIPE, input=inp).stdout.decode("utf-8").replace("\r\n", "\n")
+
+def getSourceFile(problemName):
+    files = [f for f in os.listdir(problemName) if isValidSourceFile(problemName, f)]
+
+    if(len(files) == 0):
+        print("No source file fould for problem '" + problemName + "'.\nCreate a file inside the folder matching the problem (for example '"+problemName+"/answer.py')")
+        return -1
+    
+    if(len(files) > 1):
+        print("Multiple source files found. Choose one:")
+        i = 0
+        for f in files:
+            extension = os.path.splitext(f)[1]
+            language = _LANGUAGE_GUESS[extension]
+            print(str(i+1)+") " + f + " ("+language+")")
+            i+=1
+        chosen = files[int(input()) - 1]
+        print("Running tests on " + chosen)
+        return chosen
+    
+    return files[0]
+
+def isValidSourceFile(dir, file):
+    p = os.path.join(dir, file)
+    extension = os.path.splitext(file)[1]
+    return os.path.isfile(p) and extension in _LANGUAGE_COMMANDS
+
+def getCommand(dir, file):
+    [basename, extension] = os.path.splitext(file)
+    if(extension not in _LANGUAGE_COMMANDS):
+        print("Unsupported programming language")
+        return -1
+    
+    cmd = _LANGUAGE_COMMANDS[extension]
+    path = os.path.join(dir, file)
+    return [p.replace("@f", path) for p in cmd]
+    
+def getBytesFromFile(file):
+    inFile = open(file, "rb")
+    result = inFile.read()
+    inFile.close()
+    return result
+
