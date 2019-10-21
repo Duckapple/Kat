a0bd7f026a4e2294b6abc0f07d83db752f221e3b
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2019-10-21 16:19:26 +0200

    wip

---
+++ programSelector.py
@@ -0,0 +1,144 @@
+import os, re, subprocess
+
+from fileutils import getBytesFromFile
+
+# import os, subprocess, re
+
+_LANGUAGE_GUESS = {
+    ".c": "C",
+    ".c#": "C#",
+    ".c++": "C++",
+    ".cc": "C++",
+    ".cpp": "C++",
+    ".cs": "C#",
+    ".cxx": "C++",
+    ".go": "Go",
+    ".h": "C++",
+    ".hs": "Haskell",
+    ".java": "Java",
+    ".js": "JavaScript",
+    ".m": "Objective-C",
+    ".pas": "Pascal",
+    ".php": "PHP",
+    ".pl": "Prolog",
+    ".py": "Python",
+    ".rb": "Ruby",
+}
+
+_LANGUAGE_RUN_COMMANDS = {
+    ".py": ["python", "@f"],
+    ".php": ["php", "@f"],
+    ".java": ["java", "@c"],
+    # TODO: Support rest of the languages that kattis supports
+}
+
+_LANGUAGE_COMPILE_COMMANDS = {".java": ["javac", "@f"]}
+
+_LANGUAGE_REQUIRES_CLASS = [".java"]
+
+
+def selectProgramFile(problemName):
+    # Get all files, and format them
+    files = [
+        formatProgramFile(os.path.join(problemName, f)) for f in os.listdir(problemName)
+    ]
+    # ..but only select those which we support
+    files = list(filter(isValidProgramFile, files))
+
+    if len(files) == 0:
+        print(
+            "No source file fould for problem '"
+            + problemName
+            + "'.\nCreate a file inside the folder matching the problem (for example '"
+            + problemName
+            + "/answer.py')"
+        )
+        return -1
+
+    if len(files) == 1:
+        return files[0]
+
+    print("Multiple source files found. Choose one:")
+    i = 0
+    for f in files:
+        language = guessLanguage(f)
+        print(str(i + 1) + ") " + f["name"] + " (" + language + ")")
+        i += 1
+    chosen = files[int(input("Enter number corresponding to a file: ")) - 1]
+    print("Running tests on " + chosen["name"])
+    return chosen
+
+
+def isValidProgramFile(file):
+    return (
+        os.path.isfile(file["relativePath"])
+        and file["extension"] in _LANGUAGE_RUN_COMMANDS
+    )
+
+
+def formatProgramFile(file):
+    return {
+        "relativePath": file,
+        "extension": os.path.splitext(file)[1],
+        "name": os.path.basename(file),
+    }
+
+
+def detectClassName(file):
+    content = getBytesFromFile(file["relativePath"]).decode("utf-8")
+    match = re.search("class (\w+)", content)
+    if match is None:
+        print("Could not detect class in file '" + file["name"] + "'")
+        return -1
+
+    return match.group(1).strip()
+
+
+def getRunCommand(programFile):
+    if programFile["extension"] not in _LANGUAGE_RUN_COMMANDS:
+        print("Unsupported programming language")
+        return -1
+
+    cmd = _LANGUAGE_RUN_COMMANDS[programFile["extension"]]
+
+    return [formatCommand(p, programFile) for p in cmd]
+
+
+def formatCommand(cmd, file):
+    className = (
+        ""
+        if file["extension"] not in _LANGUAGE_REQUIRES_CLASS
+        else detectClassName(file)
+    )
+    if className == -1:
+        return -1
+
+    return cmd.replace("@f", file["name"]).replace("@c", className)
+
+
+def compile(file, directory):
+    if file["extension"] not in _LANGUAGE_COMPILE_COMMANDS:
+        print("Files of this type should not be compiled")
+        return -1
+    print("Compiling " + file["name"])
+
+    cmd = [
+        formatCommand(p, file) for p in _LANGUAGE_COMPILE_COMMANDS[file["extension"]]
+    ]
+    if -1 in cmd:
+        print("Error duing compilation")
+        return -1
+
+    subprocess.run(cmd, cwd=directory)
+
+
+def shouldCompile(file):
+    return file["extension"] in _LANGUAGE_COMPILE_COMMANDS
+
+
+def guessLanguage(file):
+    return (
+        _LANGUAGE_GUESS[file["extension"]]
+        if file["extension"] in _LANGUAGE_GUESS
+        else -1
+    )
--- submit.py
+++ submit.py
@@ -1,13 +1,82 @@
-import subprocess, os
-from archive import archive
+import os, requests
+from get import promptToGet
+from programSelector import formatProgramFile, selectProgramFile, guessLanguage
+from config import getConfig
+
+_HEADERS = {"User-Agent": "Kat"}
+
 
 def submit(args, options):
-    rootDir = os.path.dirname(os.path.realpath(__file__))
-    libPath = os.path.join(rootDir, "lib_submit.py")
-    programPath = os.path.join(os.getcwd(), args[0], args[0] + ".py")
-    command = ["python", libPath, "-p", args[0], programPath]
-    if "-f" in options:
-        command.append("-f")
-    subprocess.run(args=command)
-    if "-a" in options:
-        archive(args, options)
+    problemName = args[0]
+    directory = os.path.join(os.getcwd(), problemName)
+
+    if not os.path.exists(problemName):
+        promptToGet(args, options)
+        return
+
+    # if programFile is not given, we will attempt to guess it
+    programFile = (
+        formatProgramFile(args[1]) if args[1:] else selectProgramFile(problemName)
+    )
+
+    if programFile == -1:
+        return
+
+    config = getConfig()
+
+    [cookies] = login(config)
+
+    submit_url = getUrl(config, "submissionurl", "submit")
+
+    id = postSubmission(submit_url, cookies, problemName, programFile)
+    print(id)
+    if id == -1:
+        return
+
+
+def login(config):
+    username = config.get("user", "username")
+    token = config.get("user", "token")
+    login_url = getUrl(config, "loginurl", "login")
+
+    data = {"user": username, "token": token, "script": "true"}
+
+    return requests.post(login_url, data=data, headers=_HEADERS)
+
+
+def postSubmission(url, cookies, problemName, programFile):
+    language = guessLanguage(programFile)
+    if language == -1:
+        print("Could not guess language for " + programFile)
+        return -1
+
+    data = {
+        "submit": "true",
+        "submit_ctr": 2,
+        "language": language,
+        "mainclass": mainclass,
+        "problem": problemName,
+        "script": "true",
+    }
+
+    sub_files = []
+    for f in files:
+        with open(f) as sub_file:
+            sub_files.append(
+                (
+                    "sub_file[]",
+                    (os.path.basename(f), sub_file.read(), "application/octet-stream"),
+                )
+            )
+
+    return requests.post(
+        url, data=data, files=sub_files, cookies=cookies, headers=_HEADERS
+    )
+
+
+def getUrl(cfg, option, default):
+    if cfg.has_option("kattis", option):
+        return cfg.get("kattis", option)
+    else:
+        return "https://%s/%s" % (cfg.get("kattis", "hostname"), default)
+
