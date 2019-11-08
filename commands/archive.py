c98437ac3b1dabace36b03c09ad37220c07cf6d8
Jakob Uttenthal Israelsen <jais@itu.dk>
2019-11-08 14:59:31 +0100

    GetCommand now just gets the thing, and doesn't worry about how it got there

--- commands/archive.py
+++ commands/archive.py
@@ -5,9 +5,9 @@ from helpers.exceptions import RedundantCommandException
 
 def archiveCommand(problemName, options, folder=".archive/"):
     if os.path.exists(folder + problemName):
-        raise RedundantCommandException("️️⚠️  You have already archived this problem.")
+        return
     if not os.path.exists(problemName):
         promptToGet(problemName, options)
         return
     shutil.move(problemName, folder + problemName)
-    print("📦 Moved problem", problemName, "to archive")
+    print("📦 Moved problem", problemName, "to " + folder[:-1])
