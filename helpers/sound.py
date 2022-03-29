703d5946b5d68c84f76a93d0e5158b3ba6e1bb57
jakobis <jakobisraelsen@live.dk>
2022-03-29 14:52:27 +0200

    Fixed the path problem with a hack, need to test on a computer with windows.

--- helpers/sound.py
+++ helpers/sound.py
@@ -1,7 +1,10 @@
 import glob, os, random
 from playsound import playsound
 
-prefix = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'resources')
+pathToHere = os.path.dirname(os.path.realpath(__file__))
+indexOfTrim = pathToHere.index("helpers")
+prefix = os.path.join(pathToHere[:indexOfTrim], 'resources')
+
 
 def winsound():
     playsound(random.choice(glob.glob(os.path.join(prefix, "win", "*.mp3"))))
