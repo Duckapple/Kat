804e405e9456a9def8cb7ed6a3803723debb21b7
Simon Green Kristensen <simon-siger@live.dk>
2022-09-09 16:47:42 +0200

    Better echoes

--- install.bat
+++ install.bat
@@ -1,7 +1,11 @@
 @echo off
+
+echo Installing Kat to %APPDATA%\Kat...
 powershell -Command "Invoke-WebRequest https://github.com/Duckapple/Kat/archive/refs/heads/master.zip -OutFile kat.zip"
 powershell Expand-Archive kat.zip -DestinationPath %APPDATA%
 move %APPDATA%\Kat-master %APPDATA%\Kat
+
+echo Installing Python dependencies...
 python -m pip install --user -r %APPDATA%\Kat\requirements.txt
 
 @REM env setup from <https://stackoverflow.com/questions/43934167/how-to-get-only-the-user-path-variable>
@@ -17,15 +21,16 @@ setlocal EnableDelayedExpansion
 setx Path "!UserPath!;%APPDATA%\Kat"
 endlocal
 
-echo "Kat installed!"
-echo "Install location: %APPDATA%\Kat"
-echo "Invoke via 'kattis' (runs the 'kattis.bat' in the install directory)"
-echo "Continue to open site for downloading Kattis credentials/config"
+echo Kat installed!
+echo Install location: %APPDATA%\Kat
+echo Invoke via 'kattis' (runs the 'kattis.bat' in the install directory)
+echo Continue to open site for downloading Kattis credentials/config
 pause
 
-echo "Opening browser..."
+echo Opening browser (https://open.kattis.com/download/kattisrc)...
 start https://open.kattis.com/download/kattisrc
-echo "Please save to your user directory as '.kattisrc'"
+echo Please save to your user directory as '.kattisrc'
 pause
 
 echo "Thanks for installing!"
+:EndLabel
